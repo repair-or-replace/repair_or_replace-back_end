beb591f7ac7c9388648b2129dfdfbec18c743bef
christyhernandez404 <christyhernandez404@gmail.com>
2024-10-15 17:58:11 -0400

    updating models.py

--- repair_management/models.py
+++ repair_management/models.py
@@ -3,12 +3,20 @@ from django.contrib.auth.models import User
 
 
 class Property(models.Model):
+    HOME_TYPE_CHOICES = [
+        ('single', 'Single Family'),
+        ('multi', 'Multi Family'),
+        ('condo', 'Condo'),
+        ('apartment', 'Apartment'),
+        ('other', 'Other')
+    ]
+
     address_line_1 = models.CharField(max_length=100)
     address_line_2 = models.CharField(max_length=100, blank=True, null=True)
     city = models.CharField(max_length=20)
     state = models.CharField(max_length=2)
     zipcode = models.CharField(max_length=10)
-    home_type = models.CharField(max_length=25)
+    home_type = models.CharField(max_length=25, choices=HOME_TYPE_CHOICES)
     year_built = models.DateField()
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='properties')  # link to User
     created_at = models.DateTimeField(auto_now_add=True)
@@ -17,6 +25,12 @@ class Property(models.Model):
         return f"{self.address_line_1}, {self.city}"
 
 class Appliance(models.Model):
+    STATUS_CHOICES = [
+        ('working', 'Working'),
+        ('needs repair', 'Needs Repair'),
+        ('broken', 'Broken'),
+        ('replaced', 'Replaced')
+    ]
     name = models.CharField(max_length=200)
     model = models.CharField(max_length=200)
     serial_number = models.CharField(max_length=200)
@@ -24,7 +38,7 @@ class Appliance(models.Model):
     property = models.ForeignKey('Property', on_delete=models.CASCADE, related_name='appliances')
     exp_end_of_life = models.DateField()
     purchase_date = models.DateField()
-    current_status = models.CharField(max_length=20) #
+    current_status = models.CharField(max_length=20, choices=STATUS_CHOICES) #
     cost = models.FloatField()
     created_at = models.DateTimeField(auto_now_add=True)
 
@@ -43,8 +57,14 @@ class Repairs(models.Model):
         return f"{self.appliance} ({self.repair_date})"
 
 class Investments(models.Model):
+    INVESTMENT_CHOICES = [
+        ('replacement', 'Replacement'),
+        ('maintenance', 'Maintenance'),
+        ('repair', 'Repair')
+        ('upgrade', 'Upgrade')
+    ]
     appliance = models.ForeignKey(Appliance, on_delete=models.CASCADE, related_name='investments')  # Use ForeignKey
-    investment_type = models.CharField(max_length=25)
+    investment_type = models.CharField(max_length=25, choices=INVESTMENT_CHOICES)
     investment_date = models.DateField()
     investment_description = models.CharField(max_length=250)
     cost = models.FloatField()
