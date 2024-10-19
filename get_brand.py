79df7b1b831ff88430db6b95cd24994ff7e76778
christyhernandez404 <christyhernandez404@gmail.com>
2024-10-19 13:19:03 -0400

    get brand and get product APIs working

---
+++ get_brand.py
@@ -0,0 +1,14 @@
+import requests
+
+url = "https://api.appliance-data.com/brand"
+
+headers = {
+    "accept": "application/json",
+    "Authorization": "Bearer QWtZTDlsbTUrTGhPanBLZVRKblVYaEZoN3M5V3BKYWVNOFRJVStVVk1ZUWhMM3RwOEZRUVZNZDBqNmpncFFsaXJ4TEIwZWVQMUlVQXN5RkdDNFFYOFlTTFR5TDA2NGUxWnRoeG8vbmh0QWtDOW13NUsvRTJObDdPY21oeXVnMVR3a3ZRNE1hS1NhK2ZqNGVLaitIVng3ekd3NW5HQ3g5N0hONjlpdytmM0ZEV2FUZG10c3c3QURnRStCZm9rYzB1K21pUEtEV1hvZ3UvTkJuWnlRamhjUT09"
+}
+
+response = requests.get(url, headers=headers)
+
+print(response.status_code)
+print(response.json())  # Use parentheses to properly call the method
+print(response.text)
