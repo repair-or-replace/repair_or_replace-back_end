8c092f0a8f19dad5fcc0bcc6afadec65072d67ef
james <wuab@yahoo.com>
2024-10-28 18:15:55 -0700

    update pwd

--- myproject/settings.py
+++ myproject/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'repair_or_replace',  # database
         'USER': 'root',       # username
-        'PASSWORD': 'Tomatillo4!',   # pwd
+        'PASSWORD': 'root123',   # pwd
         'HOST': 'localhost',           # local
         'PORT': '3306',                # port
     }
